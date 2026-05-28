c094c32c37f3ffd6f321f7171ac4609abab4193f
Ronald Ruzicka <ruzicka.ronald@gmail.com>
2026-05-28 15:35:29 +0200

    fix migration

--- drizzle/0002_crazy_amazoness.sql
+++ drizzle/0002_crazy_amazoness.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `list` (
 	`id` text PRIMARY KEY NOT NULL,
 	`name` text NOT NULL,
-	`user_id` text,
+	`user_id` text NOT NULL,
 	`created_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
 	`updated_at` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL,
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade
